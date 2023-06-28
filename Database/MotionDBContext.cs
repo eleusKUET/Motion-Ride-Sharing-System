8f8248b23ebf8e6682878b25de74c363cf370d86
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2023-06-28 18:19:34 +0600

    Add files via upload

---
+++ Database/MotionDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Motion.Models.Domain;
+
+namespace Motion.Database
+{
+    public class MotionDBContext : DbContext
+    {
+        public MotionDBContext(DbContextOptions options) : base(options) {}
+        public DbSet<Rider> Riders { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Ride> Rides { get; set; }
+        public DbSet<DLocation> DLocations { get; set; }
+        public DbSet<Request> Requests { get; set; }
+    }
+}
